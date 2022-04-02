6a4626f0c93472ac1ddeeaf78c9f57b9c1c63691
Gitter Dunn <gitterdunn@protonmail.com>
2022-04-02 14:51:53 +0200

    add subscription end point, introduce parameterized integration tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,7 @@
 use std::net::TcpListener;
 
 #[tokio::test]
-async fn health_check_works() {
+async fn test_health_check() {
     let address = spawn_app();
     let client = reqwest::Client::new();
 
@@ -13,6 +13,52 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn test_subscribe_returns_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Request failed to execute)");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn test_subscribe_returns_400_for_invalid_form_data() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (test_case, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "x-www-form-urlencoded")
+            .body(test_case)
+            .send()
+            .await
+            .expect("Request failed to execute");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was: {}",
+            error_message
+        )
+    }
 }
 
 fn spawn_app() -> String {
