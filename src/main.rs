ef46c1544c51e76da93941c603d61d957168a196
Gitter Dunn <gitterdunn@protonmail.com>
2022-04-09 10:03:58 +0200

    fixed tracing - stdout instead of sink

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use zero_to_prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero_to_prod".into(), "info".into(), std::io::sink);
+    let subscriber = get_subscriber("zero_to_prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Configuration not loaded");
